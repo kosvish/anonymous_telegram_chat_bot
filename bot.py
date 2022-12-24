0c5b74967b22470fd7f567671f161ca7ef9531b8
kosvish <mr.kocta@bk.ru>
2022-12-24 13:01:02 +0300

    creating a user welcome function with a menu

--- bot.py
+++ bot.py
@@ -5,6 +5,19 @@ from telebot import types
 bot = telebot.TeleBot(config.TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('Поиск собеседника')
+    markup.add(item1)
+
+    bot.send_message(
+        message.chat.id,
+        'Привет, {0.first_name}! Добро пожаловать в анонимный чат! Нажми на кнопку поиск собеседника'.format(
+            message.from_user),
+        reply_markup=markup)
+
+
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
     bot.send_message(message.chat.id, message.text)

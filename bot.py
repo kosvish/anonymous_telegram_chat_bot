2bf66ce904dd5eb8f1679a7a5aa206ccfe18cfb2
kosvish <mr.kocta@bk.ru>
2023-01-06 16:53:24 +0300

    Creating a main function to display the menu

--- bot.py
+++ bot.py
@@ -8,15 +8,23 @@ db = Database('db.db')
 bot = telebot.TeleBot(config.TOKEN)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
+def main_menu():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton('Поиск собеседника')
     markup.add(item1)
+    return markup
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('Я Парень')
+    item2 = types.KeyboardButton('Я Девушка')
+    markup.add(item1, item2)
 
     bot.send_message(
         message.chat.id,
-        'Привет, {0.first_name}! Добро пожаловать в анонимный чат! Нажми на кнопку поиск собеседника'.format(
+        'Привет, {0.first_name}! Добро пожаловать в анонимный чат! Укажи свой пол!'.format(
             message.from_user),
         reply_markup=markup)
 
@@ -33,9 +41,9 @@ def menu(message):
 @bot.message_handler(commands=['stop'])
 def stop(message):
     chat_info = db.get_active_chat(message.chat.id)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     if chat_info:
         db.delete_chat(chat_info[0])
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         item1 = types.KeyboardButton('Поиск собеседника')
         markup.add(item1)
 
@@ -71,6 +79,22 @@ def bot_message(message):
         elif message.text == 'Остановить поиск':
             db.delete_queue(message.chat.id)
             bot.send_message(message.chat.id, 'Поиск остановлен, напишите /menu')
+
+        elif message.text == 'Я Парень':
+            if db.set_gender(message.chat.id, 'male'):
+                bot.send_message(message.chat.id, 'Ваш пол успешно добавлен!', reply_markup=main_menu())
+            else:
+                bot.send_message(message.chat.id, 'Вы уже указали свой пол!')
+
+        elif message.text == 'Я Девушка':
+            if db.set_gender(message.chat.id, 'female'):
+                bot.send_message(message.chat.id, 'Ваш пол успешно добавлен!', reply_markup=main_menu())
+            else:
+                bot.send_message(message.chat.id, 'Вы уже указали свой пол!')
+
+        else:
+            chat_info = db.get_active_chat(message.chat.id)
+            bot.send_message(chat_info[1], message.text)
 
 
 bot.polling(none_stop=True)
